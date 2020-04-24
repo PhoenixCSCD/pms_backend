9d08830dea7efc9f9c5fdbfee8b55d8090854e8b
John Kennedy <developer.akabojohnkennedy@gmail.com>
2020-04-24 01:13:11 +0000

    Python Anythwere config

--- pms/settings/staging.py
+++ pms/settings/staging.py
@@ -5,11 +5,15 @@ SECRET_KEY = 'rj3dpu8-^0o0--bfjh4(wqiw0lrw67dze4bl%q#dj=ts4jp0(n'
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jkakabo.pythonanywhere.com']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'jkakabo$pms',
+        'HOST': 'jkakabo.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
+        'USER': 'jkakabo',
+        'PASSWORD': 'neutron45'
     }
 }
