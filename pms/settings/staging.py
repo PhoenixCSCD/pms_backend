f4d59b20d2946dfecdfcde12e28ab3e36c0d8d5f
John Kennedy Akabo <developer.akabojohnkennedy@gmail.com>
2020-06-02 19:33:04 +0000

    Set-up django in asgi config

--- pms/settings/staging.py
+++ pms/settings/staging.py
@@ -16,7 +16,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         'CONFIG': {
-        'hosts': [os.getenv('REDIS_URL')]
+            'hosts': [os.getenv('REDIS_URL')]
         }
     }
 }
