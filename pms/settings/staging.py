6dbbca8f09726ec10f04821d1c11e529b42e3ed9
John Kennedy Akabo <developer.akabojohnkennedy@gmail.com>
2020-06-02 10:27:17 +0000

    Setup for heroku

--- pms/settings/staging.py
+++ pms/settings/staging.py
@@ -1,19 +1,13 @@
-import os
-from .base import BASE_DIR
+from .base import *
+import dj_database_url
 
 SECRET_KEY = 'rj3dpu8-^0o0--bfjh4(wqiw0lrw67dze4bl%q#dj=ts4jp0(n'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['jkakabo.pythonanywhere.com']
+ALLOWED_HOSTS = ['phoenix-pms.herokuapp.com']
+
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'jkakabo$pms',
-        'HOST': 'jkakabo.mysql.pythonanywhere-services.com',
-        'PORT': '3306',
-        'USER': 'jkakabo',
-        'PASSWORD': 'neutron45'
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
